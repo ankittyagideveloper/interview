fd6fde1e6b595c7eb4f03dc50d2ed60195e5ee76
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2022-11-08 23:53:25 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          mY Daily Learnings
+          Continuous Daily Learnings
         </a>
       </header>
     </div>
